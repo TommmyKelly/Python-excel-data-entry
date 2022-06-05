4f0db29d776d922f7000c89b2a06364900539054
Tommy Kelly <63980664+TommmyKelly@users.noreply.github.com>
2022-06-05 21:02:11 +0100

    Added date validation

--- main.py
+++ main.py
@@ -12,49 +12,72 @@ layout = [[sg.Text('First Name'), sg.Push(), sg.Input(key="FIRST_NAME")],
           [sg.Text('Due Date'), sg.Push(), sg.Input(key='-DUE_DATE-', size=(34, 1)),
            sg.CalendarButton("Pick Date", close_when_date_chosen=True, target="-DUE_DATE-",
                              format='%d/%m/%y', no_titlebar=False, title="Pick due date")],
-          [sg.Input() ,sg.FileBrowse(file_types=(("Text Files", "*.txt"),))],
+          [sg.Text('File Attachment'), sg.Push(), sg.Input(), sg.FileBrowse(file_types=(("Text Files", "*.txt"),))],
+          [sg.Checkbox('Married', key='-MARRIED-')],
+          [sg.Checkbox('Has Children', key='-HAS_CHILDREN-')],
           [sg.Push(), sg.Button('Submit'), sg.Button('Close'), sg.Push()]]
 
 # Create the Window
 window = sg.Window('Data Entry', layout)
 
+
+def check_input(values):
+    if values['-DUE_DATE-'] == "":
+        sg.popup('No date entered')
+        return None
+    for key, value in values.items():
+        print(key)
+        print(value)
+
+    try:
+        wb = load_workbook('Book1.xlsx')
+        sheet = wb['Sheet1']
+        id_num = len(sheet['ID']) + 1
+        time_stamp = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
+        # print(time_stamp)
+
+        if values['Male']:
+            gender = 'Male'
+        else:
+            gender = 'Female'
+
+        data = [id_num, values['FIRST_NAME'],
+                values['LAST_NAME'],
+                values['NUMBER'],
+                gender,
+                values['-MARRIED-'],
+                values['-HAS_CHILDREN-'],
+                datetime.strptime(values['-DUE_DATE-'], '%d/%m/%y').strftime('%d/%m/%Y'),
+                time_stamp]
+
+        sheet.append(data)
+
+        wb.save(filename='Book1.xlsx')
+
+        # print('You entered ', values)
+
+        window["FIRST_NAME"].update(value='')
+        window["LAST_NAME"].update(value='')
+        window["NUMBER"].update(value='')
+        window["FIRST_NAME"].set_focus()
+        window["Male"].reset_group()
+        window["Female"].reset_group()
+        window["-MARRIED-"].update(False)
+        window["-HAS_CHILDREN-"].update(False)
+
+        sg.popup('Success', 'File Updated')
+    except PermissionError:
+        sg.popup('File in use', 'File in use by another user.\nPlease try again.')
+
+
 while True:
     event, values = window.read()
     print(event, values)
-    gender = None
+
     if event == sg.WIN_CLOSED or event == 'Close':  # if user closes window or clicks cancel
         break
     if event == 'Submit':
-        try:
-            wb = load_workbook('Book1.xlsx')
-            sheet = wb['Sheet1']
-            ID = len(sheet['ID']) + 1
-            time_stamp = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
-            print(time_stamp)
+        check_input(values)
 
-            if values['Male']:
-                gender = 'Male'
-            else:
-                gender = 'Female'
-
-            data = [ID, values['FIRST_NAME'], values['LAST_NAME'],
-                    values['NUMBER'], gender, datetime.strptime(values['-DUE_DATE-'],
-                                                                '%d/%m/%y').strftime('%d/%m/%Y')
-                    , time_stamp]
-
-            sheet.append(data)
-
-            wb.save(filename='Book1.xlsx')
-
-            print('You entered ', values)
-
-            window["FIRST_NAME"].update(value='')
-            window["LAST_NAME"].update(value='')
-            window["NUMBER"].update(value='')
-            window["FIRST_NAME"].set_focus()
-
-            sg.popup('Success', 'File Updated')
-        except PermissionError:
-            sg.popup('File in use', 'File in use by another user.\nPlease try again.')
 
 window.close()
